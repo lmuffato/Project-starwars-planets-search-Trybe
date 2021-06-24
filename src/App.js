89b57943ee541880e5157af035d1439c554cfd92
Renzo Sevilha <sevilharenzo@gmail.com>
2021-06-24 14:06:35 -0300

    feat: App components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+
+import Table from './components/Table';
+import FilterByName from './components/FilterByName';
+import FilterByNum from './components/FilterByNum';
+import RemoveFilters from './components/RemoveFilters';
+import OrderPlanets from './components/OrderPlanets';
+
+import { StarWarsContextProvider } from './store/contexts/StarWarsContext';
+
 import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <StarWarsContextProvider>
+      <FilterByName />
+      <FilterByNum />
+      <RemoveFilters />
+      <OrderPlanets />
+      <Table />
+    </StarWarsContextProvider>
   );
 }
 
