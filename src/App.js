967d546377af5245048edccd3dcfc8fe67868065
Gui-lira <gui.lira.martins@gmail.com>
2021-06-24 19:21:06 -0300

    5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Provider from './Provider';
 import Table from './components/Table';
 import FilterForm from './components/FilterForm';
+import FilterTable from './components/FilterTable';
 
 function App() {
   return (
     <Provider>
       <FilterForm />
+      <FilterTable />
       <Table />
     </Provider>
   );
---
+++ src/components/FilterTable.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import StarwarsContext from '../context/context';
+
+export default function FilterTable() {
+  const { filters, setFilters } = useContext(StarwarsContext);
+  const { filterByNumericValues } = filters;
+
+  const handleExcludeButton = ({ target }) => {
+    const { value } = target;
+    const filtered = filterByNumericValues.filter(({ column }) => column !== value);
+    setFilters((pastState) => ({
+      ...pastState,
+      filterByNumericValues: [...filtered],
+    }));
+  };
+
+  const mapList = () => filterByNumericValues.map((obj) => (
+    <li key={ obj.value }>
+      <p>{obj.column}</p>
+      <p>{obj.comparison}</p>
+      <p>{obj.value}</p>
+      <button
+        value={ obj.column }
+        onClick={ handleExcludeButton }
+        type="button"
+      >
+        X
+      </button>
+    </li>
+  ));
+
+  return (
+    <div>
+      <ul>
+        { mapList() }
+      </ul>
+    </div>
+  );
+}
