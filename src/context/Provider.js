7045375158f5a903d5de3685b0d3ed8a676efb9e
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-06-24 19:05:08 -0400

    feat: :zap: project restructuring

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import fetchPlanets from '../services/fetchPlanets';
 import Context from './Context';
 
 export default function Provider({ children }) {
-  const [data, setData] = useState([]);
-  // const [filterData, setFilterData] = useState([]);
-
-  const fetchApi = async () => {
-    const result = await fetchPlanets();
-    setData(result);
-    // setFilterData(result);
-  };
-
-  useEffect(() => {
-    fetchApi();
-  }, []);
-
-  console.log(data);
-
   return (
     <Context.Provider>
       {children}
