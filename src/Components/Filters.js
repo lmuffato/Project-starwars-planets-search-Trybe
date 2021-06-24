e2e0f382bafc7d27b11c58980d81f8656f5d0239
Marina Alane <marina.alane@outlook.com>
2021-06-24 17:13:33 -0300

    adc dropdown, falta funcionar

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -15,6 +15,22 @@ function Filters() {
           onChange={ handleSearchName }
         />
       </label>
+      <select data-testid="column-filter">
+        <option> population </option>
+        <option> orbital_period </option>
+        <option> diameter </option>
+        <option> rotation_period </option>
+        <option> surface_water </option>
+      </select>
+      <select data-testid="comparison-filter">
+        <option> maior que </option>
+        <option> menor que </option>
+        <option> igual a </option>
+      </select>
+      <input type="number" data-testid="value-filter" />
+      <button type="button" data-testid="button-filter" label="filter">
+        Filtrar
+      </button>
     </div>
 
   );
