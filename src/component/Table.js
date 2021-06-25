d02652b7f74b898353596532c2fb7f3eb680814d
Maria Baeta <m.martinsdg@gmail.com>
2021-06-25 20:36:34 -0300

    Fix - Requisito 2 - lint

--- src/component/Table.js
+++ src/component/Table.js
@@ -3,29 +3,27 @@ import React from 'react';
 import Data from './Data';
 import './table.css';
 
-const Table = () => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Rotação</th>
-          <th>Orbita</th>
-          <th>Diâmetro</th>
-          <th>Clima</th>
-          <th>Gravidade</th>
-          <th>Terreno</th>
-          <th>Água da superfície</th>
-          <th>População</th>
-          <th>Filmes</th>
-          <th>Criado</th>
-          <th>Editado</th>
-          <th>Url</th>
-        </tr>
-      </thead>
-      <Data />
-    </table>
-  );
-};
+const Table = () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Rotação</th>
+        <th>Orbita</th>
+        <th>Diâmetro</th>
+        <th>Clima</th>
+        <th>Gravidade</th>
+        <th>Terreno</th>
+        <th>Água da superfície</th>
+        <th>População</th>
+        <th>Filmes</th>
+        <th>Criado</th>
+        <th>Editado</th>
+        <th>Url</th>
+      </tr>
+    </thead>
+    <Data />
+  </table>
+);
 
 export default Table;
