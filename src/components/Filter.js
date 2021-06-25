c708571f6143d7d227c12d30a099e8c6acfdb1fa
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-25 18:19:53 -0300

    feat: create a filter system by input text, and input infos - req 3

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 function Filter() {
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState(0);
+
   const { filters, setFilters } = useContext(PlanetContext);
+  const { filterByNumericValues } = filters;
 
   const handleChange = (event) => {
-    setFilters({ filterByName: { name: event.target.value } });
+    setFilters({ ...filters, filterByName: { name: event.target.value } });
   };
 
-  const activeFilters = () => (Object.keys(filters).length !== 0 ? (
+  const handleChangeColumn = (event) => {
+    setColumn(event.target.value);
+  };
+
+  const handleChangeComparison = (event) => {
+    setComparison(event.target.value);
+  };
+
+  const handleChangeValue = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const filterNumbers = {
+      column,
+      comparison,
+      value,
+    };
+
+    if (!filterByNumericValues) {
+      setFilters({
+        ...filters,
+        filterByNumericValues: [filterNumbers],
+      });
+    } else {
+      setFilters({
+        ...filters,
+        filterByNumericValues: filterByNumericValues.concat(filterNumbers),
+      });
+    }
+  };
+
+  const activeFilters = () => (Object.keys(filters).includes('filterByName') ? (
     <div>
-      <span>Active Filters</span>
+      <span>Active Name Filter</span>
       <p>{ `filters: { filterByName: { ${filters.filterByName.name} } }`}</p>
+    </div>
+  ) : <p>Nenhum filtro ativo!</p>
+  );
+
+  const activeNumberFilters = () => (filters.filterByNumericValues.length >= 1 ? (
+    <div>
+      <span>Active Number Filters</span>
+      { filters.filterByNumericValues
+        .map((filter) => (
+          <p key={ filter.column }>
+            { ` { filterByNumericValues: { 
+              ${filter.column},
+              ${filter.comparison},
+              ${filter.value} } }`}
+          </p>)) }
     </div>
   ) : <p>Nenhum filtro ativo!</p>
   );
@@ -19,7 +73,36 @@ function Filter() {
   return (
     <div>
       <input type="text" data-testid="name-filter" onChange={ handleChange } />
+
+      <div>
+        <select data-testid="column-filter" onChange={ handleChangeColumn }>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+
+        <select data-testid="comparison-filter" onChange={ handleChangeComparison }>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+
+        <input type="text" data-testid="value-filter" onChange={ handleChangeValue } />
+
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
+        >
+          Adicionar filtro
+        </button>
+      </div>
+
       { activeFilters() }
+      { filters.filterByNumericValues ? activeNumberFilters() : null }
+
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,7 @@ import PlanetContext from '../context/PlanetContext';
 
 function Table() {
   const { data, isLoading, filters } = useContext(PlanetContext);
+  const { filterByNumericValues } = filters;
 
   const tableHeader = () => {
     const headers = data.map((planet) => {
@@ -25,13 +26,57 @@ function Table() {
     }
   };
 
+  const createTable = (info) => (info.map((planet) => (
+    <tr key={ planet.name }>
+      <td>{planet.name}</td>
+      <td>{planet.rotation_period}</td>
+      <td>{planet.orbital_period}</td>
+      <td>{planet.diameter}</td>
+      <td>{planet.climate}</td>
+      <td>{planet.gravity}</td>
+      <td>{planet.terrain}</td>
+      <td>{planet.surface_water}</td>
+      <td>{planet.population}</td>
+      <td>
+        {
+          planet.films
+            .map((film, index) => (<ul key={ index }><li>{ film }</li></ul>))
+        }
+      </td>
+      <td>{planet.created}</td>
+      <td>{planet.edited}</td>
+      <td>{planet.url}</td>
+    </tr>
+  )));
+
   const tableContent = () => {
     if ('filterByName' in filters) {
       const testFilter = data
         .filter((planets) => planets.name.includes(filters.filterByName.name));
 
       return (
-        testFilter.map((planet) => (
+        createTable(testFilter)
+      );
+    }
+
+    if (filterByNumericValues) {
+      const numericFilter = filterByNumericValues.map((filterPlanet) => {
+        const { column, comparison, value } = filterPlanet;
+
+        switch (comparison) {
+        case 'maior que':
+          return data.filter((planets) => Number(planets[column]) > Number(value));
+        case 'menor que':
+          return data.filter((planets) => Number(planets[column]) < Number(value));
+        case 'igual a':
+          return data.filter((planets) => Number(planets[column]) < Number(value));
+        default:
+          return data;
+        }
+      });
+
+      return (
+        numericFilter[0].map((planet) => (
           <tr key={ planet.name }>
             <td>{planet.name}</td>
             <td>{planet.rotation_period}</td>
@@ -55,7 +100,6 @@ function Table() {
         ))
       );
     }
-
     return (
       data.map((planet) => (
         <tr key={ planet.name }>
@@ -95,3 +139,5 @@ function Table() {
 }
 
 export default Table;
+
+// Consegui aplicar o switch case olhando o código do Rafael Gerônimo Link: https://github.com/tryber/sd-010-a-project-starwars-planets-search/pull/22/commits/6eac2978f8afed9f109bd2500079ce13ecda89d9
