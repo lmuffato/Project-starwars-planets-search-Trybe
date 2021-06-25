25e9ed04a943f5a222acea857dc26a8daa4c698e
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-25 19:37:32 -0300

    fix: refatoring more lines

--- src/components/Table.js
+++ src/components/Table.js
@@ -76,28 +76,8 @@ function Table() {
       });
 
       return (
-        numericFilter[0].map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>
-              {
-                planet.films
-                  .map((film, index) => (<ul key={ index }><li>{ film }</li></ul>))
-              }
-            </td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))
+        numericFilter.map((entrie) => createTable(entrie))
+        // createTable(numericFilter[0])
       );
     }
     return (
