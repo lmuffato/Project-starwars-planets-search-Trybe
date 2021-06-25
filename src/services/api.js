589e8deaac685ceb384ea3db47f1fdf0d2c384c2
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-06-25 19:54:00 -0300

    Feat: (In comment) Renderizar todos os 60 planetas da API

--- src/services/api.js
+++ src/services/api.js
@@ -5,12 +5,34 @@ const URL_FILM_4 = '4';
 const URL_FILM_5 = '5';
 const URL_FILM_6 = '6';
 
+// const fetchPlanetsPages = async (page) => {
+//   const response = await fetch(`https://swapi-trybe.herokuapp.com/api/planets/?page=${page}`);
+//   const data = await response.json();
+//   const { results } = data;
+//   return results;
+// };
+
 const fetchPlanets = async () => {
   const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
   const data = await response.json();
-  const { results } = data;
+  const page1 = data.results;
+
+  // Renderizar todos os 60 planetas (porém não passa no test):
+
+  // const page2 = await fetchPlanetsPages(URL_FILM_2);
+  // const page3 = await fetchPlanetsPages(URL_FILM_3);
+  // const page4 = await fetchPlanetsPages(URL_FILM_4);
+  // const page5 = await fetchPlanetsPages(URL_FILM_5);
+  // const page6 = await fetchPlanetsPages(URL_FILM_6);
+  const results = page1;
+  // .concat(page2
+  // .concat(page3
+  // .concat(page4
+  // .concat(page5
+  // .concat(page6)))));
   await results.forEach(async (result) => {
     const { films } = result;
+    if (films.length < 1) films[0] = 'https://swapi-trybe.herokuapp.com/api/films/0';
     let index = 0;
     films.forEach((film) => {
       film = film.replace(/([^\d])+/gim, ''); // https://pt.stackoverflow.com/questions/3719/como-obter-apenas-os-n%C3%BAmeros-de-uma-string-em-javascript
@@ -34,6 +56,7 @@ const fetchPlanets = async () => {
         films[index] = 'Revenge of the Sith';
         break;
       default:
+        films[index] = 'None';
         break;
       }
       index += 1;
