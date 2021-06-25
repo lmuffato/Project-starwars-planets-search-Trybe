a31ad5750f3e43f10718fc1c7cc7b4e692e4a6b9
Gui-lira <gui.lira.martins@gmail.com>
2021-06-25 18:10:52 -0300

    refatoração provider

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-// import Provider from './Provider';
+import Provider from './Provider';
 import Table from './components/Table';
 import FilterForm from './components/FilterForm';
 import FilterTable from './components/FilterTable';
-import StarwarsContext from './context/context';
-
-const columnFilters = [
-  'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
-];
 
 function App() {
-  const [data, setData] = useState([]);
-  const [filters, setFilters] = useState({
-    filterByName: {
-      name: '',
-    },
-    filterByNumericValues: [],
-  });
-  const [columnFiltersState] = useState(columnFilters);
-
-  useEffect(() => {
-    const fetchApi = async () => {
-      const starwarsApi = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-      const json = await starwarsApi.json();
-      // console.log(json);
-      const { results } = json;
-      results.forEach((planets) => delete planets.residents);
-      setData(results);
-    };
-    fetchApi();
-  }, []);
-
-  const context = {
-    data,
-    filters,
-    setFilters,
-    columnFiltersState,
-  };
   return (
-    <StarwarsContext.Provider value={ context }>
+    <Provider>
       <FilterForm />
       <FilterTable />
       <Table />
-    </StarwarsContext.Provider>
+    </Provider>
   );
 }
 
