4c01a5daeaecad91d0c1c2c540569872d5c7a08d
Giovanni <giovannitadeu08@gmail.com>
2021-06-24 21:11:14 -0300

    fix: lint

--- src/services/dataApi.js
+++ src/services/dataApi.js
@@ -2,9 +2,13 @@ const ENDPOINT = 'https://swapi-trybe.herokuapp.com/api/planets/';
 /* const ENDPOINT_TEST = 'https://swapi.dev/api/planets'; */
 
 const requestAPI = async () => {
-  const requestFetch = await fetch(ENDPOINT);
-  const response = await requestFetch.json();
-  return response;
+  try {
+    const requestFetch = await fetch(ENDPOINT);
+    const response = await requestFetch.json();
+    return response;
+  } catch (error) {
+    throw error('A requisição falhou!');
+  }
 };
 
 export default requestAPI;
