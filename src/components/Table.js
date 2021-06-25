ac334f26d16e4c7f4af9650d2bfe42af1f649a04
Marcelo Maurício <mmauricio.design@gmail.com>
2021-06-25 19:31:26 -0300

    fix: refatoring some repeated lines - req 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -101,28 +101,7 @@ function Table() {
       );
     }
     return (
-      data.map((planet) => (
-        <tr key={ planet.name }>
-          <td>{planet.name}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.population}</td>
-          <td>
-            {
-              planet.films
-                .map((film, index) => (<ul key={ index }><li>{ film }</li></ul>))
-            }
-          </td>
-          <td>{planet.created}</td>
-          <td>{planet.edited}</td>
-          <td>{planet.url}</td>
-        </tr>
-      ))
+      createTable(data)
     );
   };
 
