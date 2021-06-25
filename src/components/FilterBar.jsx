75e17338d5247ee6e6a90f79e2a61e6f1271dd8e
Lucas André <lucas.cancado89@gmail.com>
2021-06-25 18:41:31 -0300

    Req05

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -17,61 +17,87 @@ function FilterBar() {
       filterByNumericValues: filters.filterByNumericValues.concat({
         column: columnFilter, comparison: comparisonFilter, value: valueFilter }) });
     setFilterArr(filterArr.filter((e) => e !== columnFilter));
+    setColumnFilter('');
+  }
+
+  function removeFilter(e) {
+    const filterToRemove = e.target.name;
+    const newFilters = filters.filterByNumericValues
+      .filter((el) => el.column !== filterToRemove);
+    setFilters({
+      ...filters,
+      filterByNumericValues: newFilters,
+    });
+    setFilterArr(filterArr.concat(filterToRemove));
   }
 
   return (
-    <form>
-      <label htmlFor="planetName">
-        Planet
-        <input
-          type="text"
-          id="planetName"
-          name="planetName"
-          data-testid="name-filter"
-          onChange={
-            (e) => (
-              setFilters({
-                ...filters, filterByName: { name: e.target.value },
-              }))
-          }
-        />
-      </label>
-      <select
-        data-testid="column-filter"
-        value={ columnFilter }
-        onChange={ (e) => (setColumnFilter(e.target.value)) }
-      >
-        {filterArr.map((e) => (
-          (<option key={ e } name={ e } value={ e }>{e}</option>)
-        ))}
-      </select>
-      <select
-        data-testid="comparison-filter"
-        value={ comparisonFilter }
-        onChange={ (e) => (setComparisonFilter(e.target.value)) }
-      >
-        <option name="maior que" value="maior que">maior que</option>
-        <option name="menor que" value="menor que">menor que</option>
-        <option name="igual a" value="igual a">igual a</option>
-      </select>
-      <label htmlFor="quantity">
-        <input
-          id="quantity"
-          name="quantity"
-          type="number"
-          value={ valueFilter }
-          data-testid="value-filter"
-          onChange={ (e) => setValueFilter(e.target.value) }
-        />
-      </label>
-      <button
-        data-testid="button-filter"
-        type="button"
-        onClick={ handleClick }
-      >
-        Adicionar Filtro
-      </button>
-    </form>
+    <>
+      <form>
+        <label htmlFor="planetName">
+          Planet
+          <input
+            type="text"
+            id="planetName"
+            name="planetName"
+            data-testid="name-filter"
+            onChange={
+              (e) => (
+                setFilters({
+                  ...filters, filterByName: { name: e.target.value },
+                }))
+            }
+          />
+        </label>
+        <select
+          data-testid="column-filter"
+          value={ columnFilter }
+          onChange={ (e) => (setColumnFilter(e.target.value)) }
+        >
+          {filterArr.map((e) => (
+            (<option key={ e } name={ e } value={ e }>{e}</option>)
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          value={ comparisonFilter }
+          onChange={ (e) => (setComparisonFilter(e.target.value)) }
+        >
+          <option name="maior que" value="maior que">maior que</option>
+          <option name="menor que" value="menor que">menor que</option>
+          <option name="igual a" value="igual a">igual a</option>
+        </select>
+        <label htmlFor="quantity">
+          <input
+            id="quantity"
+            name="quantity"
+            type="number"
+            value={ valueFilter }
+            data-testid="value-filter"
+            onChange={ (e) => setValueFilter(e.target.value) }
+          />
+        </label>
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ handleClick }
+        >
+          Adicionar Filtro
+        </button>
+      </form>
+      {filters.filterByNumericValues.map((el) => (
+        <div data-testid="filter" key={ el.column } className="added-filters">
+          <p key={ el.column }>{ `${el.column} ${el.comparison} ${el.value}` }</p>
+          <button
+            onClick={ (e) => removeFilter(e) }
+            type="button"
+            name={ el.column }
+          >
+            X
+          </button>
+        </div>
+      ))}
+    </>
   );
 }
 
