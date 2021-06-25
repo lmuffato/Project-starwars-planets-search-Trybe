01fb52f0261dc0da989db46a32b46bbad62d0451
guidugaich <guidugaich.dev@gmail.com>
2021-06-25 17:18:31 -0300

    adding provider to App and importing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Provider from './context/Provider';
+import Table from './components/Table';
+import Filters from './components/Filters';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <Filters />
+      <Table />
+    </Provider>
   );
 }
 
