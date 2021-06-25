9c51a67157d5265f15f045a2a3a9d3086dd77555
guidugaich <guidugaich.dev@gmail.com>
2021-06-25 18:18:59 -0300

    adding logic to update name filter in context

--- src/components/Filters/NameFilter.jsx
+++ src/components/Filters/NameFilter.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import useFilters from '../../hooks/useFilters';
 
 export default function NameFilter() {
+  const { filters, setFilters } = useFilters();
+
   return (
     <div>
       <label htmlFor="name-filter">
         Name Filter:
-        <input id="name-filter" data-testid="name-filter" />
+        <input
+          type="text"
+          id="name-filter"
+          data-testid="name-filter"
+          onChange={ (event) => setFilters({
+            ...filters,
+            filterByName: { name: event.target.value },
+          }) }
+        />
       </label>
     </div>
   );
