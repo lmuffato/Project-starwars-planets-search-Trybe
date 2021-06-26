1d7322205623df80c93bfe46eae0fcea5ba05b7e
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-06-26 20:20:53 -0300

    fix: requisito 1 - final

--- src/components/ListPlanets.js
+++ src/components/ListPlanets.js
@@ -2,46 +2,46 @@ import React, { useEffect, useState } from 'react';
 import planetsAPI from '../services/planetsAPI';
 
 const ListPlanets = () => {
-  // const [planet, setPlanets] = useState('');
   const [planets, setData] = useState([]);
 
   useEffect(() => {
     planetsAPI().then(({ results }) => setData(results));
   }, []);
 
-  console.log(planets);
-  // planets.then();
-  // console.log(Object.keys(planets[0]));
-
   return (
     <section>
-      {planets.length > 0
-      && <table>
-        <thead>
-          <tr>
-            {Object.keys(planets[0])
-              .filter((headers) => (headers !== 'residents'))
-              .map((headers) => (
-                <th key={ headers } value={ headers }>{headers}</th>
-              ))}
-          </tr>
-          {/* <tr>
-            {Object.keys(planets[0]).filter((headers) => (headers !== 'residents'))
-              .map((headers) => (
-                <th key={ headers } value={ headers }>{headers}</th>
-              ))}
-          </tr> */}
-        </thead>
-        <tbody>
-          {/* {this.listExpenses()} */}
-        </tbody>
-        {/* {planets.map((planet) => (
-          console.log(Object.keys(planet))
-          // const keys = (Object.keys(planet))
-          // <li key={ planet.name }>{planet.name}</li>
-        ))} */}
-      </table>}
-      {/* {planets.length <= 0 && 'Loading...' } */}
+      {planets.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(planets[0])
+                .filter((headers) => (headers !== 'residents'))
+                .map((headers) => (
+                  <th key={ headers } value={ headers }>{headers}</th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {planets.length > 0 && planets.map((planet) => (
+              <tr key={ planet.name }>
+                <td value={ planet.name }>{planet.name}</td>
+                <td value={ planet.rotation_period }>{planet.rotation_period}</td>
+                <td value={ planet.orbital_period }>{planet.orbital_period}</td>
+                <td value={ planet.diameter }>{planet.diameter}</td>
+                <td value={ planet.climate }>{planet.climate}</td>
+                <td value={ planet.gravity }>{planet.gravity}</td>
+                <td value={ planet.terrain }>{planet.terrain}</td>
+                <td value={ planet.surface_water }>{planet.surface_water}</td>
+                <td value={ planet.population }>{planet.population}</td>
+                <td value={ planet.films }>{planet.films}</td>
+                <td value={ planet.created }>{planet.created}</td>
+                <td value={ planet.edited }>{planet.edited}</td>
+                <td value={ planet.url }>{planet.url}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   );
 };
