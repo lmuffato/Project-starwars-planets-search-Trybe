4ab5db21e8bef59d81fcb8f923056ef317e149c8
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-06-25 20:31:25 -0400

    feat: :sparkles: Implementing Requirement 3

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -13,6 +13,30 @@ export default function Inputs() {
           placeholder="Nome do Planeta"
           onChange={ handleFilterChange }
         />
+        <select data-testid="column-filter" id="column-filter">
+          <option>population</option>
+          <option>orbital_period</option>
+          <option>diameter</option>
+          <option>rotation_period</option>
+          <option>surface_water</option>
+        </select>
+        <select data-testid="comparison-filter" id="comparison-filter">
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          data-testid="value-filter"
+          id="value-filter"
+          type="number"
+        />
+        <button
+          data-testid="button-filter"
+          id="button-filter"
+          type="button"
+        >
+          Buscar
+        </button>
       </label>
     </form>
   );
