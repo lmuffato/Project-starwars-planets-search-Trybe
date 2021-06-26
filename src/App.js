0ed86383bc5a02de921ca5ef0fb5b979121bc680
renancosta21 <renancosta21@gmail.com>
2021-06-26 18:47:04 -0300

    req1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Planets from './Planets';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Planets />
   );
 }
 
---
+++ src/Planets.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+export default function PlanetsList() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets')
+      .then((response) => response.json())
+      .then((data) => {
+        setPlanets(data.results);
+      });
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      {planets.map((planet) => (
+        <tbody key={ planet.name }>
+          <tr>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  );
+}
