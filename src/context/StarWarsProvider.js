1be08edc59ed2de25b1ca4d118e2f3f1b4fa05b9
Alexandre Oliveira <alexandremolive@gmail.com>
2021-06-26 12:26:22 -0300

    req 3 done

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
 import getStarWarsPlanets from './services/starWarsApi';
 import StartWarsContext from './StarWarsContext';
 
