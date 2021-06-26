1d3fad9e450a79235902c3cebdfc1378dc4b2c58
guidugaich <guidugaich.dev@gmail.com>
2021-06-26 16:16:17 -0300

    logic to render only necessary fields

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 
 import NameFilter from './Filters/NameFilter';
 import NumericFilter from './Filters/NumericFilter';
+import AppliedNumericFilter from './Filters/AppliedNumericFilter';
+import useFilters from '../hooks/useFilters';
 
 export default function Filters() {
+  const { filters } = useFilters();
+  const appliedFilters = filters.filterByNumericValues;
+  const appliedFiltersCategories = appliedFilters.map((filter) => filter.column);
+
+  const numericFilterCategories = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+
+  const availableNumericFilterCategories = numericFilterCategories.filter(
+    (filterCat) => !appliedFiltersCategories.includes(filterCat),
+  );
+
   return (
     <div>
       <NameFilter />
-      <NumericFilter />
+      {appliedFilters.map((filter) => (
+        <AppliedNumericFilter appliedNumFilter={ filter } key={ filter.column } />
+      ))}
+      <NumericFilter available={ availableNumericFilterCategories } />
     </div>
   );
 }
