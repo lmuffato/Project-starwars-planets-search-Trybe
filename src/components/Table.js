b0032c7aad00b6fb1be1f8a8014a08845ed5dc36
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-06-27 20:29:59 -0300

    erro lint

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,25 +6,23 @@ function Table() {
     backup } = useContext(myContext);
   const resultNames = Object.keys(data[0]);
   const filtered = resultNames.filter((name) => name !== 'residents');
-  const renderizePlanets = (array) => {
-    return array.map((planet) => (
-      <tr key={ planet.name }>
-        <td>{planet.name}</td>
-        <td>{planet.rotation_period}</td>
-        <td>{planet.orbital_period}</td>
-        <td>{planet.diameter}</td>
-        <td>{planet.climate}</td>
-        <td>{planet.gravity}</td>
-        <td>{planet.terrain}</td>
-        <td>{planet.surface_water}</td>
-        <td>{planet.population}</td>
-        <td>{planet.films}</td>
-        <td>{planet.created}</td>
-        <td>{planet.edited}</td>
-        <td>{planet.url}</td>
-      </tr>
-    ));
-  };
+  const renderizePlanets = (array) => array.map((planet) => (
+    <tr key={ planet.name }>
+      <td>{planet.name}</td>
+      <td>{planet.rotation_period}</td>
+      <td>{planet.orbital_period}</td>
+      <td>{planet.diameter}</td>
+      <td>{planet.climate}</td>
+      <td>{planet.gravity}</td>
+      <td>{planet.terrain}</td>
+      <td>{planet.surface_water}</td>
+      <td>{planet.population}</td>
+      <td>{planet.films}</td>
+      <td>{planet.created}</td>
+      <td>{planet.edited}</td>
+      <td>{planet.url}</td>
+    </tr>
+  ));
   return (
     <div>
       <table>
