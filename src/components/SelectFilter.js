9c4c66a63ade9e1c722ef68bca2c1f260361f14e
Patrick Morais <ppternunes@gmail.com>
2021-06-28 18:48:56 -0300

    requisito 4

--- src/components/SelectFilter.js
+++ src/components/SelectFilter.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import { StarWarsContext } from '../provider/Provider';
 
 const SelectFilter = ({ setFilters, filters, setData }) => {
-  const { data } = React.useContext(StarWarsContext);
-  const [inputFilters, setInputFilters] = React.useState({});
-  const valuesArray = [
+  const valuesArrayInit = [
     'population',
     'orbital_period',
     'diameter',
     'rotation_period',
     'surface_water',
   ];
+  const { data } = React.useContext(StarWarsContext);
+  const [inputFilters, setInputFilters] = React.useState({});
+  const [valuesArray, setValuesArray] = React.useState(valuesArrayInit);
 
   const valueFor = ['maior que', 'menor que', 'igual a'];
 
@@ -21,6 +22,12 @@ const SelectFilter = ({ setFilters, filters, setData }) => {
       [name]: value,
     });
   };
+
+  React.useEffect(() => {
+    const filteredValues = valuesArray
+      .filter((elem) => elem !== inputFilters.column);
+    setValuesArray(filteredValues);
+  }, [inputFilters]);
 
   const valuesForFilter = () => {
     setFilters({
@@ -36,9 +43,7 @@ const SelectFilter = ({ setFilters, filters, setData }) => {
       if (inputFilters.comparison === 'menor que') {
         return elem[column] < +(inputFilters.value);
       }
-      if (inputFilters.comparison === 'igual a') {
-        return elem[column] === inputFilters.value;
-      }
+      return elem[column] === inputFilters.value;
     });
     setData(dataFiltered);
   };
@@ -46,7 +51,6 @@ const SelectFilter = ({ setFilters, filters, setData }) => {
   return (
     <div>
       <select data-testid="column-filter" name="column" onChange={ handleChangeSelect }>
-        <option>Select option</option>
         {valuesArray
           .map((element) => <option value={ element } key={ element }>{element}</option>)}
       </select>
