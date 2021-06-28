d42662edec6eb81e7461023fefb081e855588aa9
Patrick Morais <ppternunes@gmail.com>
2021-06-28 13:13:41 -0300

    requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,22 +3,41 @@ import { StarWarsContext } from '../provider/Provider';
 
 const Table = () => {
   const data = React.useContext(StarWarsContext);
+  const [filterByName, setFilterByName] = React.useState('');
   console.log(Object.keys({ ...data[0] }));
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {Object.keys({ ...data[0] }).map((title) => (<th key={ title }>{title}</th>))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((elem) => (
+  const thead = () => (
+    <thead>
+      <tr>
+        {Object.keys({ ...data[0] }).map((title) => (<th key={ title }>{title}</th>))}
+      </tr>
+    </thead>
+  );
+
+  const tbody = () => (
+    <tbody>
+      {data.filter((planet) => planet.name.toLowerCase().includes(filterByName))
+        .map((elem) => (
           <tr key={ elem.name }>
             {Object.values(elem)
               .map((item) => <td key={ item }>{item}</td>)}
           </tr>))}
-      </tbody>
+    </tbody>
+  );
+
+  const inputSearch = () => (
+    <input
+      data-testid="name-filter"
+      value={ filterByName }
+      onChange={ (e) => setFilterByName(e.target.value) }
+    />
+  );
+
+  return (
+    <table>
+      { inputSearch() }
+      { thead() }
+      { tbody() }
     </table>
   );
 };
