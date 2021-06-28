bd178c5ebe9ebda03583abb880bf334554d59a27
Gustavo Sousa <gustavohl98@gmail.com>
2021-06-28 12:06:23 -0300

    Req 1 - Implemented table with fetched data (planets)

---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import PlanetsContext from './PlanetsContext';
+
+function PlanetsProvider(props) {
+  const { children } = props;
+  const [APIData, setAPIData] = useState({});
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const res = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const data = await res.json();
+      setAPIData(data);
+    };
+
+    fetchPlanets();
+  }, []);
+
+  return (
+    <PlanetsContext.Provider value={ { data: APIData } }>
+      { children }
+    </PlanetsContext.Provider>
+  );
+}
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.shape(),
+}.isRequired;
+
+export default PlanetsProvider;
