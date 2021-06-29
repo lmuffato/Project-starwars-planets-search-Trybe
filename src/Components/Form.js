73b1d7f23b122b20dc1eab037f083b6dcba2f75d
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-06-29 17:20:16 -0300

    Desafio 3 em andamento

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -16,6 +16,38 @@ function Form() {
           onChange={ (ev) => handleChangeNamePlanet(ev) }
         />
       </label>
+      <label htmlFor="column-filter">
+        <select id="column-filter" data-testid="column-filter">
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="comparison-filter">
+        <select id="comparison-filter" data-testid="comparison-filter">
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+      </label>
+      <label htmlFor="value-filter">
+        Valor
+        {' '}
+        <input
+          id="value-filter"
+          type="number"
+          data-testid="value-filter"
+        />
+      </label>
+      <button
+        id="button-filter"
+        type="submit"
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
     </form>
   );
 }
