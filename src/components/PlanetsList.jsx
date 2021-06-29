1229caf5643826f572088fc11fbcc88e7790682d
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-06-29 02:56:11 -0400

    fix: bugs w/table role

--- src/components/PlanetsList.jsx
+++ src/components/PlanetsList.jsx
@@ -7,58 +7,58 @@ function PlanetsList() {
   const { filteredPlanets, planets, getPlanets } = useContext(PlanetsContext);
   useEffect(() => { getPlanets(); }, [getPlanets]);
   return (
-    <div>
-      <span>
-        <th>
-          Name
-        </th>
-        <th>
-          Population
-        </th>
-        <th>
-          Climate
-        </th>
-        <th>
-          Terrain
-        </th>
-        <th>
-          Orbital Period
-        </th>
-        <th>
-          Diameter
-        </th>
-        <th>
-          Gravity
-        </th>
-        <th>
-          Rotation Period
-        </th>
-        <th>
-          Surface Water
-        </th>
-        <th>
-          Films
-        </th>
-        <th>
-          Created in
-        </th>
-        <th>
-          Edited in
-        </th>
-        <th>
-          Link
-        </th>
-      </span>
-      <table>
-        <tbody>
-          {
-            planets ? filteredPlanets
-              .map((planet, index) => <PlanetCard key={ index } planet={ planet } />)
-              : <tr><td> ...loading </td></tr>
-          }
-        </tbody>
-      </table>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Name
+          </th>
+          <th>
+            Population
+          </th>
+          <th>
+            Climate
+          </th>
+          <th>
+            Terrain
+          </th>
+          <th>
+            Orbital Period
+          </th>
+          <th>
+            Diameter
+          </th>
+          <th>
+            Gravity
+          </th>
+          <th>
+            Rotation Period
+          </th>
+          <th>
+            Surface Water
+          </th>
+          <th>
+            Films
+          </th>
+          <th>
+            Created in
+          </th>
+          <th>
+            Edited in
+          </th>
+          <th>
+            Link
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          planets ? filteredPlanets
+            .map((planet, index) => <PlanetCard key={ index } planet={ planet } />)
+            : <tr><td> ...loading </td></tr>
+        }
+      </tbody>
+    </table>
   );
 }
 
