2aa2db6acaf8bb3c05c856b5cf9a2fdb3fb6c229
biazidioti <bzferreira.bio@gmail.com>
2021-06-29 08:39:39 -0300

    implements Context and Provider, and creates table.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <span>Hello, Bia!</span>
+    <>
+      <span>Hello, Bia!</span>
+      <thead>
+        <td>Cat1</td>
+        <td>Cat1</td>
+      </thead>
+    </>
   );
 }
 
---
+++ src/context/PlanetsProvider.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import PlanetContext from './PlanetsContext';
+
+function PlanetProvider() {
+  const [planetCategories] = useState({
+    cat1: [],
+    cat2: [],
+  });
+return(
+  
+)
+}
