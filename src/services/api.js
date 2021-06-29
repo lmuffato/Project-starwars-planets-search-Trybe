508215a2b8158a64952409de58dbb8b7c37ede26
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-06-29 12:20:12 -0300

    feat:req01

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+const api = async () => {
+  const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+  const data = await response.json();
+  return data;
+};
+
+export default api;
