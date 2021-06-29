080138c5bcc43c984f1f9e64e6750a712255ae9f
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-06-28 22:48:47 -0300

    fix: NaN

--- src/App.test.js
+++ src/App.test.js
@@ -28,7 +28,7 @@ const mockFetch = () => {
     }));
 }
 
-describe('1 - Faça uma requisição para o endpoint `/planets` da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna `residents`', () => {
+describe.only('1 - Faça uma requisição para o endpoint `/planets` da API de Star Wars e preencha uma tabela com os dados retornados, com exceção dos da coluna `residents`', () => {
   beforeAll(mockFetch);
   beforeEach(cleanup);
 
@@ -338,7 +338,7 @@ describe('5 - Apague o filtro de valores numéricos e desfaça as filtragens dos
   });
 });
 
-describe.only('6 - Ordene as colunas de forma ascendente ou descendente', () => {
+describe('6 - Ordene as colunas de forma ascendente ou descendente', () => {
   beforeAll(mockFetch);
   beforeEach(cleanup);
 
--- src/services/requestPlanetsAPI.js
+++ src/services/requestPlanetsAPI.js
@@ -12,14 +12,19 @@ const requestPlanetsAPI = async () => {
       delete result.residents;
       return ({
         name: result.name,
-        rotation_period: +result.rotation_period,
-        orbital_period: +result.orbital_period,
-        diameter: +result.diameter,
+        rotation_period: !Number
+          .isNaN(+result.rotation_period) ? +result.rotation_period : 'unknown',
+        orbital_period: !Number
+          .isNaN(+result.orbital_period) ? +result.orbital_period : 'unknown',
+        diameter: !Number
+          .isNaN(+result.diameter) ? +result.diameter : 'unknown',
         climate: result.climate,
         gravity: result.gravity,
         terrain: result.terrain,
-        surface_water: +result.surface_water,
-        population: +result.population,
+        surface_water: !Number
+          .isNaN(+result.surface_water) ? +result.surface_water : 'unknown',
+        population: !Number
+          .isNaN(+result.population) ? +result.population : 'unknown',
         films: result.films,
         created: result.created,
         edited: result.edited,
