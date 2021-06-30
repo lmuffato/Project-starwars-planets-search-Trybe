15c03e29ff64d6568d53f6c5f1f2401063aa3edf
lmiremuffato <lmiremuffato@gmail.com>
2021-06-30 10:52:05 -0300

    requisito1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ServiceApi from './components/ServiceApi';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <div>
+      <ServiceApi />
+    </div>
   );
 }
 
