8ece55ad8ebf0991b2858c69837c9e0421162550
mallmann02 <leomallmann020302@gmail.com>
2021-07-01 19:38:02 -0300

    feat: req3

---
+++ src/utils/selectFieldsData.js
@@ -0,0 +1,29 @@
+export const columnFilterOption = [
+  {
+    name: 'population',
+  },
+  {
+    name: 'orbital_period',
+  },
+  {
+    name: 'diameter',
+  },
+  {
+    name: 'rotation_period',
+  },
+  {
+    name: 'surface_water',
+  },
+];
+
+export const comparisonFilterOption = [
+  {
+    comparison: 'maior que',
+  },
+  {
+    comparison: 'menor que',
+  },
+  {
+    comparison: 'igual a',
+  },
+];
