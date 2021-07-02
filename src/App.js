df9549e92e55b1dfe84eca6952477a32c3ff55f1
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-02 16:35:52 -0300

    Req 5 - implemented filter remotion feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
 import FilterComponent from './components/FilterComponent';
+import FilterList from './components/FilterList';
 
 function App() {
   const DOCUMENT_TITLE = 'Star Wars Planets Search';
@@ -12,6 +13,7 @@ function App() {
     <PlanetsProvider>
       <h1>{ DOCUMENT_TITLE }</h1>
       <FilterComponent />
+      <FilterList />
       <Table />
     </PlanetsProvider>
   );
---
+++ src/components/FilterList.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function FilterList() {
+  const { filters: { filterByNumericValues },
+    setfilterByNumericValues } = useContext(PlanetsContext);
+
+  // Lógica de remoção de despesa inspirada na resolução do exercício
+  // de Redux do freeCodeCamp: Remove an Item from an Array
+  // bem como conforme colocado no meu projeto trybewallet
+  // https://github.com/tryber/sd-010-a-project-trybewallet/blob/gustavofds-project-trybewallet/src/reducers/wallet.js
+
+  function onClick(index) {
+    setfilterByNumericValues(
+      filterByNumericValues
+        .slice(0, index)
+        .concat(filterByNumericValues.slice(index + 1)),
+    );
+  }
+
+  return (
+    <div>
+      { filterByNumericValues.map((filter, index) => (
+        <p key={ index } data-testid="filter">
+          <span>{ `${filter.column} ${filter.comparison} ${filter.value}` }</span>
+          { ' ' }
+          <button onClick={ () => onClick(index) } type="button">X</button>
+        </p>
+      )) }
+    </div>
+  );
+}
+
+export default FilterList;
