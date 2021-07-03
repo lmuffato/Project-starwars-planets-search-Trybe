e8f76b0dfb7e572ba2b9a0aaa321b8d1914c9f1c
NotebookWin7 <lmiremuffato@gmail.com>
2021-07-02 23:29:03 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import ServiceApi from './components/services/ServiceApi';
 import PlanetProvider from './context/PlanetProvider';
-import InputTextFilter from './components/input/InputTextFilter';
 import Table from './components/Table';
 
 function App() {
--- src/components/services/ServiceApi.jsx
+++ src/components/services/ServiceApi.jsx
@@ -4,22 +4,21 @@ import PlanetContext from '../../context/PlanetContext';
 function ServiceApi() {
   const { setPlanetsList, setTableColumns } = useContext(PlanetContext);
 
-  const getPlanets = async () => {
-    try {
-      const urlApi = 'https://swapi-trybe.herokuapp.com/api/planets/';
-      const response = await fetch(urlApi);
-      const { results } = await response.json();
-      if (results.length !== 0 || results === undefined) {
-        setPlanetsList(results);
-        setTableColumns(Object.keys(results[0]));
-      }
-    } catch (error) { console.error(error); }
-  };
-
   // DidMount
   useEffect(() => {
+    const getPlanets = async () => {
+      try {
+        const urlApi = 'https://swapi-trybe.herokuapp.com/api/planets/';
+        const response = await fetch(urlApi);
+        const { results } = await response.json();
+        if (results.length !== 0 || results === undefined) {
+          setPlanetsList(results);
+          setTableColumns(Object.keys(results[0]));
+        }
+      } catch (error) { console.error(error); }
+    };
     getPlanets();
-  }, []);
+  }, [setPlanetsList, setTableColumns]);
 
   return (
     null
