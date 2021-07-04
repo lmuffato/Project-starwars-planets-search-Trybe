a86cf2865ca283d58d9adcf28c0ebba3b4f394c4
Tiago Santos <tiago881@gmail.com>
2021-07-04 17:32:00 -0300

    Agradecimentos

--- src/App.js
+++ src/App.js
@@ -12,5 +12,13 @@ function App() {
     </Provider>
   );
 }
-
+/*
+Este projeto foi construído com colaboração dos meu colegas da Turma-10 Tribo-A.
+Gostaria de deixar meus agradecimentos ao envolvidos por ter compartilhado seus
+conhecimentos.
+Guilherme Dornelles | https://github.com/guilhermemd
+André Barroso | https://github.com/AndreBarroso
+Michael Petterson | https://github.com/michael-petterson-06
+Felipe Müller | https://github.com/felipemuller20
+*/
 export default App;
