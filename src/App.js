d098adfa5dfb2ad8c282a08d431650503443ffc6
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-05 06:15:39 -0300

    fix: req 3

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Provider from './context/Provider';
 import Table from './components/Table';
 import PlanetFilter from './components/PlanetFilter';
-import NumberFilter from './components/NumberFilter';
+import NumbersFilters from './components/NumberFilter';
 
 function App() {
   return (
     <Provider>
       <PlanetFilter />
-      <NumberFilter />
+      <NumbersFilters />
       <Table />
     </Provider>
   );
--- src/components/NumberFilter.js
+++ src/components/NumberFilter.js
@@ -20,47 +20,43 @@ function NumbersFilters() {
 
   return (
     <form>
-      <label htmlFor="column">
-        Filtra Colunas:
-        <select
-          name="column"
-          data-testid="column-filter"
-          value={ handleChange }
-        >
-          <option>column</option>
-          <option value="surface_water">surface_water</option>
-          <option value="orbital_period">orbital_period</option>
-          <option value="population">population</option>
-          <option value="diameter">diameter</option>
-          <option value="rotation_period">rotation_period</option>
-        </select>
-      </label>
-      <label htmlFor="comparison">
-        <select
-          name="comparison"
-          data-testid="comparison-filter"
-          onChange={ handleChange }
-        >
-          <option>comparison</option>
-          <option value=">">maior que</option>
-          <option value="<">menor que</option>
-          <option value="===">igual a</option>
-        </select>
-      </label>
-      <label htmlFor="value">
-        <input
-          type="number"
-          name="value"
-          data-testid="value-filter"
-          onChange={ handleChange }
-        />
-      </label>
+      <select
+        name="column"
+        data-testid="column-filter"
+        onChange={ handleChange }
+      >
+        <option>select</option>
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+
+      <select
+        name="comparison"
+        data-testid="comparison-filter"
+        onChange={ handleChange }
+      >
+        <option>select</option>
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+
+      <input
+        type="number"
+        name="value"
+        data-testid="value-filter"
+        onChange={ handleChange }
+      />
+
       <button
         type="button"
         data-testid="button-filter"
         onClick={ handleClick }
       >
-        Adiciona Filtro
+        Add Filters
       </button>
     </form>
 
