fb97d968d281385391e17ab5d487e010309dfb4c
lmiremuffato <lmiremuffato@gmail.com>
2021-07-06 01:39:05 -0300

    requisito 5

--- src/components/input/InputFilters.jsx
+++ src/components/input/InputFilters.jsx
@@ -9,7 +9,7 @@ function InputTextFilter() {
   const {
     filterByName, setFilterByName,
     filterByNumericValues,
-    // setFilterByNumericValues,
+    setFilterByNumericValues,
     // // filters,
     // setFilters,
     columnFilter,
@@ -37,14 +37,19 @@ function InputTextFilter() {
 
   const disponibleOptionsFunc = () => {
     const disponibleOptions = options.filter((option) => !usedOptions.includes(option));
-    console.log(disponibleOptions);
     return disponibleOptions
-      .map((option) => (<option key={ option } value={ option }>{option}</option>));
+      .map((option) => (
+        <option key={ option } value={ option }>{option}</option>
+      ));
+  };
+
+  const removeFilters = () => {
+    setFilterByNumericValues(['']);
   };
 
   return (
     <span>
-      <span>
+      <span data-testid="filter">
         <h1>{filterByName}</h1>
         <input
           type="text"
@@ -52,45 +57,53 @@ function InputTextFilter() {
           value={ filterByName }
           onChange={ (event) => setFilterByName(event.target.value) }
         />
+        <button type="button" onClick={ removeFilters }>X</button>
       </span>
-      <h1>{columnFilter}</h1>
-      <label htmlFor="column-filter">
-        Filter for column
-        <select
-          id="column-filter"
-          data-testid="column-filter"
-          name="column-filter"
-          value={ columnFilter }
-          onChange={ (event) => setColumnFilter(event.target.value) }
-        >
-          {disponibleOptionsFunc()}
-        </select>
-      </label>
-
-      <label htmlFor="comparison-filter">
-        Comparison Filter
-        <select
-          id="comparison-filter"
-          data-testid="comparison-filter"
-          name="comparison-filter"
-          value={ comparisonFilter }
-          onChange={ (event) => setComparisonFilter(event.target.value) }
-        >
-          <option value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <label htmlFor="value-filter">
-        Value filter
-        <input
-          id="value-filter"
-          data-testid="value-filter"
-          type="number"
-          value={ valueFilter }
-          onChange={ (event) => setValueFilter(event.target.value) }
-        />
-      </label>
+      <span data-testid="filter">
+        <label htmlFor="column-filter">
+          Filter for column
+          <select
+            id="column-filter"
+            data-testid="column-filter"
+            name="column-filter"
+            value={ columnFilter }
+            onChange={ (event) => setColumnFilter(event.target.value) }
+          >
+            {disponibleOptionsFunc()}
+          </select>
+        </label>
+        <button type="button" onClick={ removeFilters }>X</button>
+      </span>
+      <span data-testid="filter">
+        <label htmlFor="comparison-filter">
+          Comparison Filter
+          <select
+            id="comparison-filter"
+            data-testid="comparison-filter"
+            name="comparison-filter"
+            value={ comparisonFilter }
+            onChange={ (event) => setComparisonFilter(event.target.value) }
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <button type="button" onClick={ removeFilters }>X</button>
+      </span>
+      <span data-testid="filter">
+        <label htmlFor="value-filter">
+          Value filter
+          <input
+            id="value-filter"
+            data-testid="value-filter"
+            type="number"
+            value={ valueFilter }
+            onChange={ (event) => setValueFilter(event.target.value) }
+          />
+        </label>
+        <button type="button" onClick={ removeFilters }>X</button>
+      </span>
       <div>
         <button
           id="button-filter"
