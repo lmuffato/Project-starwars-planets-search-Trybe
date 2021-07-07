c5b0dbec6148d2db2971b1ac139c6178ec9ddf8b
jepaur5 <jepaur5@gmail.com>
2021-07-07 00:40:42 -0300

    requisito 3 e 4

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,20 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StarWarsContext } from '../contexts/StarWarsProvider';
 
-const Filter = () => {
-  const { filters: { filterByName: { name } }, setName } = useContext(StarWarsContext);
+const columns = ['population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
 
-  const handleChange = ({ target }) => {
-    const { value } = target;
-    setName(value);
-  };
+const comparisons = ['maior que', 'menor que', 'igual a'];
+
+const Filter = () => {
+  const { filters, setName, setFiltersByNumericValues } = useContext(StarWarsContext);
+  const { filterByNumericValues } = filters;
+
+  const [column, setColumn] = useState(columns[0]);
+  const [comparison, setComparison] = useState(comparisons[0]);
+  const [value, setValue] = useState('');
+  const [unavailableFilters, setUnavailableFilters] = useState([]);
 
   return (
-    <input
-      data-testid="name-filter"
-      value={ name }
-      onChange={ handleChange }
-    />
+    <>
+      <input
+        data-testid="name-filter"
+        onChange={ ({ target: { value: valueName } }) => setName(valueName) }
+      />
+      <select
+        data-testid="column-filter"
+        onChange={ ({ target: { value: valueColumn } }) => setColumn(valueColumn) }
+      >
+        {columns.filter((col) => !unavailableFilters.includes(col))
+          .map((col) => <option value={ col } key={ col }>{ col }</option>)}
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ ({ target: { value: valueComparison } }) => (
+          setComparison(valueComparison)) }
+      >
+        {comparisons.map((comp) => (
+          <option value={ comp } key={ comp }>{ comp }</option>))}
+      </select>
+
+      <input
+        data-testid="value-filter"
+        onChange={ ({ target: { value: valueNumber } }) => setValue(valueNumber) }
+      />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        disabled={ !value }
+        onClick={ () => {
+          setFiltersByNumericValues([...filterByNumericValues,
+            { column, comparison, value }]);
+          unavailableFilters.push(column);
+          setUnavailableFilters([...unavailableFilters]);
+          setColumn(columns.filter((col) => !unavailableFilters.includes(col))[0]);
+        } }
+      >
+        Filtrar
+      </button>
+    </>
   );
 };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,8 +2,23 @@ import React, { useContext } from 'react';
 import { StarWarsContext } from '../contexts/StarWarsProvider';
 
 const Table = () => {
-  const { planets, filters: { filterByName: { name } } } = useContext(StarWarsContext);
+  const { planets, filters } = useContext(StarWarsContext);
+  const { filterByName: { name }, filterByNumericValues } = filters;
   const headers = planets[0] || [];
+
+  const comparingBetween = (valueA, valueB, comparison) => {
+    if (typeof (valueA) === 'number' || typeof (valueB) === 'number') return false;
+
+    valueA = parseFloat(valueA);
+    valueB = parseFloat(valueB);
+
+    if (comparison === 'maior que') return valueA > valueB;
+    if (comparison === 'menor que') return valueA < valueB;
+    if (comparison === 'igual a') return valueA === valueB;
+
+    return true;
+  };
+
   return (
     <table>
       <thead>
@@ -16,9 +31,14 @@ const Table = () => {
       </thead>
       <tbody>
         {
-          planets.filter((planet) => (name ? (planet.name).includes(name) : true))
-            .map((planet) => (
-              <tr key={ planet.name }>
+          filterByNumericValues
+            .reduce((acc, { column, value, comparison }) => acc
+              .filter((planet) => (
+                comparingBetween(planet[column], value, comparison))),
+            planets)
+            .filter((planet) => (planet.name).includes(name))
+            .map((planet, index) => (
+              <tr key={ index }>
                 {
                   Object.values(planet)
                     .map((info) => <td key={ info }>{info}</td>)
