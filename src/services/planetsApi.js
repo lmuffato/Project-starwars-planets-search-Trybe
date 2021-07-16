920e0255de78fbfbec9f3e2e50fcf9a72942efc2
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-07-15 21:57:38 -0300

    Ne pussivi

--- src/services/planetsApi.js
+++ src/services/planetsApi.js
@@ -1,12 +1,6 @@
-export default function fetchPlanets() {
-  return fetch('https://swapi-trybe.herokuapp.com/api/planets/')
-    .then((response) => response.json())
-    .then((data) => data.results)
-    .catch((error) => error);
-}
-
 export function fetchByColumn() {
-  return fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/'
+  return fetch(url)
     .then((response) => response.json())
     .then((data) => Object.keys(data.results[0]).filter((key) => key !== 'residents'))
     .catch((error) => error);
