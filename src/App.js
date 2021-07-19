fb2956eae61a0adfc6219a78d955681ef28b4a2d
Apoc35 <alexv_silveira@outlook.com>
2021-07-19 05:00:33 -0300

    All requisits.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import TableOfPlanets from './Components/TableOfPlanets';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <TableOfPlanets />
+    </Provider>
   );
 }
 
---
+++ src/context/Provider.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { node } from 'prop-types';
+import myContext from './myContext';
+
+const Provider = ({ children }) => {
+  const [data, changeData] = useState([]);
+  const [name, changeFilterName] = useState('');
+  const [numericFilters, changeNumericFilters] = useState([]);
+  const [filteredPlanets, changeFilteredPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((result) => {
+        result.json()
+          .then(({ results }) => {
+            changeData(results);
+          });
+      });
+  }, []);
+
+  useEffect(() => {
+    let filtered = data.filter((planet) => planet.name.includes(name));
+    numericFilters.forEach(({ column, comparison, value }) => {
+      if (column && comparison) {
+        switch (comparison) {
+        case 'maior que':
+          filtered = filtered.filter((planet) => parseFloat(planet[column]) > value);
+          break;
+        case 'menor que':
+          filtered = filtered.filter((planet) => parseFloat(planet[column]) < value);
+          break;
+        case 'igual a':
+          filtered = filtered.filter((planet) => parseFloat(planet[column]) === value);
+          break;
+        default:
+          break;
+        }
+      }
+    });
+    changeFilteredPlanets(filtered);
+  }, [name, data, numericFilters]);
+
+  const addNumericFilter = ({ column, comparison, value }) => {
+    changeNumericFilters([
+      ...numericFilters,
+      { column, comparison, value },
+    ]);
+  };
+
+  const deleteNumericFilter = (column) => {
+    const filters = [];
+    numericFilters.forEach((filter) => {
+      if (filter.column !== column) {
+        filters.push(filter);
+      }
+    });
+    changeNumericFilters(filters);
+  };
+
+  const value = {
+    data: filteredPlanets,
+    filters: {
+      filterByName: {
+        name,
+      },
+      filterByNumericValues: [
+        ...numericFilters,
+      ],
+    },
+    changeFilterName,
+    addNumericFilter,
+    deleteNumericFilter,
+  };
+
+  return (
+    <myContext.Provider value={ value }>
+      { children }
+    </myContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: node,
+}.isRequired;
+
+export default Provider;
