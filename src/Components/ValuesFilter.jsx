6369269fed237c8624f216f8ac9a69fc599b69f2
Southier <patrick.southier@hotmail.com>
2021-07-20 11:31:35 -0300

    feat: completes requirement 5. special thanks to Lucas Pedroso, who helped me with to solve my problem with the test

--- src/Components/ValuesFilter.jsx
+++ src/Components/ValuesFilter.jsx
@@ -9,6 +9,14 @@ function ValuesFilter() {
     setFilterOptions,
   } = useContext(planetsContext);
 
+  let columnsArr = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+
   const handleChange = ({ target }) => {
     const { value, id } = target;
     setFilterOptions({
@@ -33,13 +41,19 @@ function ValuesFilter() {
     });
   };
 
-  let columnsArr = [
-    'population',
-    'orbital_period',
-    'diameter',
-    'rotation_period',
-    'surface_water',
-  ];
+  const handleDelete = (columnName) => {
+    const { filterByNumericValues } = filters;
+    const newFilterObj = filterByNumericValues.filter((currFilter) => (
+      currFilter.column !== columnName
+    ));
+    setFilters({
+      ...filters,
+      filterByNumericValues: [
+        ...newFilterObj,
+      ],
+    });
+    columnsArr.push(columnName);
+  };
 
   const { filterByNumericValues } = filters;
 
@@ -61,35 +75,58 @@ function ValuesFilter() {
   }
 
   return (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="column">
-        Column
-        <select id="column" data-testid="column-filter" onChange={ handleChange }>
-          {columnsArr.map((column) => (
-            <option key={ column } value={ column }>{ column }</option>
-          ))}
-        </select>
-      </label>
-      <label htmlFor="comparison">
-        Comparison
-        <select id="comparison" data-testid="comparison-filter" onChange={ handleChange }>
-          <option selected value="maior que">maior que</option>
-          <option value="menor que">menor que</option>
-          <option value="igual a">igual a</option>
-        </select>
-      </label>
-      <label htmlFor="value">
-        Value
-        <input
-          id="value"
-          type="number"
-          value={ filterOptions.value }
-          data-testid="value-filter"
-          onChange={ handleChange }
-        />
-      </label>
-      <button type="submit" data-testid="button-filter">Submit Filter</button>
-    </form>
+    <div>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="column">
+          Column
+          <select id="column" data-testid="column-filter" onChange={ handleChange }>
+            {columnsArr.map((column) => (
+              <option key={ column } value={ column }>{ column }</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="comparison">
+          Comparison
+          <select
+            id="comparison"
+            data-testid="comparison-filter"
+            onChange={ handleChange }
+          >
+            <option selected value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="value">
+          Value
+          <input
+            id="value"
+            type="number"
+            value={ filterOptions.value }
+            data-testid="value-filter"
+            onChange={ handleChange }
+          />
+        </label>
+        <button type="submit" data-testid="button-filter">Submit Filter</button>
+      </form>
+      <h4>Current Filters</h4>
+      {filterByNumericValues.length >= 1 ? filterByNumericValues.map((filter) => (
+        <div key={ filter.column } data-testid="filter">
+          <span>{ filter.column }</span>
+          {' '}
+          <span>{ filter.comparison }</span>
+          {' '}
+          <span>{ filter.value }</span>
+          {' '}
+          <button
+            type="button"
+            onClick={ () => handleDelete(filter.column) }
+          >
+            X
+          </button>
+        </div>
+      )) : <span>No filters set yet</span>}
+    </div>
   );
 }
 
